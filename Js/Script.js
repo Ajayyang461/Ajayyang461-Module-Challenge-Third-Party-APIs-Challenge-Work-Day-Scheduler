8b15df12ec9189b418aa58fa36f2f24a53b77123
A-Jay Yang <103970240+Ajayyang461@users.noreply.github.com>
2022-07-06 16:15:39 -0500

    added time display and local storage

--- Js/Script.js
+++ Js/Script.js
@@ -1,4 +1,14 @@
+//displaying current time on top
 var currentDayEl = document.querySelector("#currentDay");
 currentDayEl.textContent = moment().format("dddd MMM Do YY");
 
-$(".saveBtn").on("click", function () {});
+//create buttone to save vaule to the local storage
+$(".saveBtn").on("click", function () {
+  // get the the value once click
+  var getValue = $(this).siblings(".description").val();
+  var getTime = $(this).parent().attr("id");
+
+  // saves the value in the local storage
+  localStorage.setItem(getTime, getValue);
+});
+
